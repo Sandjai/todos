02d7ed9bac221bd1ebd92e5ee80b080447cbe9c7
Grigory Semakin <grinsdot1@gmail.com>
2023-04-02 14:19:07 +0300

    single todo generator added

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 import "./styles/main.sass";
+import todosData from "./fixtures/tododata.json";
+import todoItem from "./components/todoItem.pug";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const list = document.getElementById("js-list");
+  const elementUl = document.createElement("ul");
+
+  if (todosData && Array.isArray(todosData)) {
+    for (let todo of todosData) {
+      elementUl.insertAdjacentHTML("beforeend", todoItem({ todo }));
+    }
+  }
+
+  list.append(elementUl);
+
+  for (let item of document.querySelectorAll(".item__label")) {
+    item.addEventListener("click", (e) => {
+      e.target.closest("div").classList.toggle("item__text_completed");
+    });
+  }
+});
