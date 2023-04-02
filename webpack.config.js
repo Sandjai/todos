2b189cdf173a00e855b8f20b731d499b392dde03
Grigory Semakin <grinsdot1@gmail.com>
2023-04-02 08:01:00 +0300

    setup of webpack

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: {
+    main: path.resolve(__dirname, "./src/index.js"),
+  },
+
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "[name].bundle.js",
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "To do list",
+      template: path.resolve(__dirname, "./src/index.pug"),
+      filename: "index.html",
+    }),
+    new CleanWebpackPlugin(),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        type: "asset/inline",
+      },
+      {
+        test: /\.(sass|scss|css)$/,
+        use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"],
+      },
+      {
+        test: /\.pug$/,
+        loader: "pug-loader",
+      },
+    ],
+  },
+
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+
+    compress: true,
+    port: 9000,
+  },
+};
